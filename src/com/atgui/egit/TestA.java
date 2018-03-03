b6602f0bea21f72329d05945398787f5aff3c4a4
liudong <chrisliu0130@aliyun.com>
2018-03-03 20:39:14 +0800

    123123

--- src/com/atgui/egit/TestA.java
+++ src/com/atgui/egit/TestA.java
@@ -1,11 +1,10 @@
 package com.atgui.egit;
 
-import java.net.StandardSocketOptions;
-
 public class TestA {
 
 	public static void main(String[] args) {
 		System.out.println("Test eeee");
+		System.out.println(123123);
 	}
 
 }
